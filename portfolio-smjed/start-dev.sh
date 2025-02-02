7fbb209b2c79612785dce2db4bf661fa915095fb
Sabri MJAHED <contact@sabrimjahed.com>
2025-02-02 20:05:46 +0100

    Add initial project structure and configuration files

---
+++ portfolio-smjed/start-dev.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hugo server --baseURL="http://127.0.0.1" --bind=127.0.0.1 --environment dev --navigateToChanged
---
+++ portfolio-smjed/start-prod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+hugo server --baseURL="https://smjed.net/" --bind=192.168.31.215 --disableFastRender --environment production --appendPort=false &
